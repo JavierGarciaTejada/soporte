8c2ce2f857ccc87e5f64bd62922dddfb0edfe56c
Javier Garcia <javigar31@gmail.com>
2019-10-18 19:41:43 -0500

    Exportar ciertas columnas excel

--- media/js/reporte.js
+++ media/js/reporte.js
@@ -116,7 +116,13 @@ $(function(){
 		lengthChange: false,
 		dom: 'Bfrtip',
         buttons: [
-	        'excel'
+	        {
+	        	extend: 'excelHtml5',
+		        exportOptions: {
+	                columns: [1,2,3,4,5,6,7,8,9,10,11]
+	            }
+	        }
+	        
 	    ],
 		"columns" : [
 			{
