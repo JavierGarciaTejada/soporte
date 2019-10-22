018e0c962a550bd14e6727be9e69edd7a55649eb
Javier Garcia <javigar31@gmail.com>
2019-10-22 18:00:24 -0500

    Columnas exportar excel

--- media/js/reporte.js
+++ media/js/reporte.js
@@ -147,7 +147,7 @@ $(function(){
 	        {
 	        	extend: 'excelHtml5',
 		        exportOptions: {
-	                columns: [1,2,3,4,5,6,7,8,9,10,11]
+	                columns: [1,2,3,4,5,6,7,8,9,10,11,12,13]
 	            }
 	        }
 	        
