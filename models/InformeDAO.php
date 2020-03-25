d4853041ae39428dc34aa8f96600e93e155160ba
Garcia Tejada Francisco Javier <FGTEJADA@telmex.com>
2020-03-24 19:05:25 -0600

    Filtro dashboard PROMEDIO

--- models/InformeDAO.php
+++ models/InformeDAO.php
@@ -96,14 +96,14 @@ class InformeDAO
 
 	public static function InformePromedio(){
 		#WHERE estado = 'Liquidado'
-		$sql = "SELECT if(evento <> 'Falla', evento, if(impacto LIKE '%SA', 'Falla sin afectación', 'Falla con afectación')) evento, ROUND(AVG(TIMESTAMPDIFF(SECOND, fecha_soporte, fecha_fin_falla) / 3600),2) tiempo FROM bitacora GROUP BY if(evento <> 'Falla', evento, if(impacto LIKE '%SA', 'Falla sin afectacion', 'Falla con afectacion'))";
+		$sql = "SELECT if(evento <> 'Falla', evento, if(impacto LIKE '%SA', 'FALLA SIN AFECTACION', 'FALLA CON AFECTACION')) evento, ROUND(AVG(TIMESTAMPDIFF(SECOND, fecha_soporte, fecha_fin_falla) / 3600),2) tiempo FROM bitacora GROUP BY if(evento <> 'Falla', evento, if(impacto LIKE '%SA', 'Falla sin afectacion', 'Falla con afectacion'))";
 		$data = self::executeQuery($sql);
 		return $data;
 	}
 
 	public static function InformePromedioFiltrado($filtro){
 		$fil = self::Filtro($filtro);
-		$sql = "SELECT if(evento <> 'Falla', evento, if(impacto LIKE '%SA', 'Falla sin afectacion', 'Falla con afectacion')) evento, ROUND(AVG(TIMESTAMPDIFF(SECOND, fecha_soporte, fecha_fin_falla) / 3600),2) tiempo, c.cl gerencia FROM bitacora a LEFT join si_usr b on id_ingeniero = b.id LEFT join ad_sig c on b.cl = c.ix $fil GROUP BY if(evento <> 'Falla', evento, if(impacto LIKE '%SA', 'Falla sin afectacion', 'Falla con afectacion'))";
+		$sql = "SELECT if(evento <> 'Falla', evento, if(impacto LIKE '%SA', 'FALLA SIN AFECTACION', 'FALLA CON AFECTACION')) evento, ROUND(AVG(TIMESTAMPDIFF(SECOND, fecha_soporte, fecha_fin_falla) / 3600),2) tiempo, c.cl gerencia FROM bitacora a LEFT join si_usr b on id_ingeniero = b.id LEFT join ad_sig c on b.cl = c.ix $fil GROUP BY if(evento <> 'Falla', evento, if(impacto LIKE '%SA', 'Falla sin afectacion', 'Falla con afectacion'))";
 
 		// $sql = "SELECT evento, ROUND(AVG(TIMESTAMPDIFF(SECOND, fecha_soporte, fecha_fin_falla) / 3600),2) tiempo, c.cl gerencia FROM bitacora a inner join si_usr b on usuario_captura = b.id inner join ad_sig c on b.cl = c.ix $fil GROUP BY evento";
 		$data = self::executeQuery($sql);
